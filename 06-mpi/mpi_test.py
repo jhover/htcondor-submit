dfde48edf009ac73484fc71863ffa89625d4afc0
jhover <jhover@cloudy.acf.bnl.gov>
2016-10-28 11:31:07 -0400

    Update.

--- 06-mpi/mpi_test.py
+++ 06-mpi/mpi_test.py
@@ -5,6 +5,7 @@
 # John Hover <jhover@bnl.gov>
 import glob
 import os
+import socket
 import sys
 import time
 
@@ -21,22 +22,31 @@ sys.path.append('/usr/lib64/python%s.%s/site-packages/mpich' % (major, minor))
 
 print os.environ.get("_CONDOR_SCRATCH_DIR", None)
 cwd = os.getcwd()
-os.makedirs("x/y/z")
-os.chdir("x/y/z")
+hostname = socket.getfqdn()
+#os.makedirs("x/y/z")
+#os.chdir("x/y/z")
 print glob.glob("~/*")
-print glob.glob("*")
-os.chdir(cwd)
+#print glob.glob("*")
+#os.chdir(cwd)
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
 print "Hello from node %d ( %d ): %s" % (rank, size, str(glob.glob("*")))
-os.makedirs("{}".format(size))
+#os.makedirs("{}".format(size))
 print "Hello from node %d ( %d ): %s" % (rank, size, str(glob.glob("*")))
 
 if rank == 0:
-    time.sleep(3)
+    time.sleep(300)
+    print "Node %s is controller..." % hostname 
     for rt,dirs,files in os.walk(cwd):
         for f in dirs + files:
             print path.join(rt, f)
+else:
+    time.sleep(300)
+    print "Node %s is slave..." % hostname 
+    for rt,dirs,files in os.walk(cwd):
+        for f in dirs + files:
+            print path.join(rt, f)    
+    
